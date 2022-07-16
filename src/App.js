eb345e8fc0fed41d7af14a47df5e60b9d4ae9cc9
Gregory Richens <gregoryrichens@gmail.com>
2022-07-16 09:39:41 -0600

    Update repository to point to correct project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import './App.css';
-import { useSelector, useDispatch } from 'react-redux'
+import Nav from './components/Nav'
+import ContentWrapper from './components/ContentWrapper'
+import Footer from './components/Footer'
 
 function App() {
-  // your logic goes here!
-
+  
   return (
-    <div className="App">
-      <div>
-        <button onClick={() => {}}>Trigger Thunk</button>
-        <button onClick={() => {}}>Clear</button>
-        <button onClick={() => {}}>Next</button>
-        <button onClick={() => {}}>Back</button>
-      </div>
-      <input onChange={(e) => { }} />
-      <div>
-        {/* Once you have plugged everything in, render the image here! */}
-      </div>
+    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
+      <Nav />
+      <ContentWrapper />
+      <Footer />
     </div>
   );
 }
